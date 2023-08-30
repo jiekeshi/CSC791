ce20207613fb74c053aceb4b33243c81d230a200
Jieke SHI <jiekeshi125@gmail.com>
2023-08-31 00:00:54 +0800

    fix: fix data convert issure

--- utils.py
+++ utils.py
@@ -107,6 +107,35 @@ class InputFeatures(object):
         self.soft_label = soft_label
 
 
+class TextDataset(Dataset):
+    def __init__(self, tokenizer, file_path, logger):
+        self.examples = []
+        logger.info("Creating features from file at %s ", file_path)
+
+        data = []
+        with open(file_path) as f:
+            for line in f:
+                data.append(json.loads(line.strip()))
+
+        for d in tqdm(data):
+            self.examples.append(convert_examples_to_features(d, tokenizer))
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        return torch.tensor(self.examples[i].input_ids), torch.tensor(self.examples[i].label)
+
+def convert_examples_to_features(data, tokenizer):
+    code = " ".join(data["func"].split())
+    code_tokens = tokenizer.tokenize(code)[:400-2]
+    source_tokens = [tokenizer.cls_token]+code_tokens+[tokenizer.sep_token]
+    source_ids = tokenizer.convert_tokens_to_ids(source_tokens)
+    padding_length = 400 - len(source_ids)
+    source_ids += [tokenizer.pad_token_id]*padding_length
+    return InputFeatures(source_tokens, source_ids, data["target"])
+
+
 # deprecated this class
 class Token_Encoder(object):
     def __init__(self, vocab_size, encoding, file_path, logger):
